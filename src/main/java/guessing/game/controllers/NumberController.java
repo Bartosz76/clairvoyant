8c7a48f8358ded17d17af2236baf800932bc5664
Bartosz <mierzwinski.b@gmail.com>
2019-12-31 00:23:40 +0100

    service logic finished

--- src/main/java/guessing/game/controllers/NumberController.java
+++ src/main/java/guessing/game/controllers/NumberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class NumberController {
 
@@ -15,15 +17,39 @@ public class NumberController {
     @Autowired
     NumberService numberService;
 
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    public String firstResult(Model model){
+        int generatedNumber = numberService.random();
+        model.addAttribute("generatedNumber", generatedNumber);
+        return "firstGuess";
+    }
+
+    @RequestMapping(value = "/no", method = RequestMethod.POST)
+    public String triedAgain(Model model){
+        int generatedAgain = numberService.random();
+        model.addAttribute("generatedAgain", generatedAgain);
+        return "tryagain";
+    }
+
     @RequestMapping("/home")
     public String home(){ return "home";}
 
-    @RequestMapping(value = "/home", method = RequestMethod.POST)
-    public String firstResult(Model model){
-    int generatedNumber = numberService.random();
-    model.addAttribute("generatedNumber", generatedNumber);
-    return "firstGuess";
+   @RequestMapping(value = "/firstGuess", method = RequestMethod.GET)
+   public String firstGues(){return "firstGuess";}
 
-    }
+    @RequestMapping(value = "/no", method = RequestMethod.GET)
+    public String answeredNo(){return "no";}
+
+    @RequestMapping(value = "/yes", method = RequestMethod.GET)
+    public String answeredYes(){return "yes";}
+
+    @RequestMapping(value = "/tryagain", method = RequestMethod.GET)
+    public String tryAgain(){return "tryagain";}
+
+    @RequestMapping(value = "/failed", method = RequestMethod.GET)
+    public String failed(){return "failed";}
+
+
+
 
 }
