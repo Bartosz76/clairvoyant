723d6d8b35dc7f83ed8a36bac98edd80d8ede869
Bartosz <mierzwinski.b@gmail.com>
2020-04-26 11:29:19 +0200

    fixed the mapping and thymeleaf connections

--- src/main/java/guessing/game/controllers/NumberController.java
+++ src/main/java/guessing/game/controllers/NumberController.java
@@ -6,50 +6,53 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class NumberController {
+public class NumberController{
 
 
     @Autowired
     NumberService numberService;
 
     @RequestMapping(value = "/home", method = RequestMethod.POST)
-    public String firstResult(Model model){
+    public String firstResult(Model model) {
         int generatedNumber = numberService.random();
         model.addAttribute("generatedNumber", generatedNumber);
         return "firstGuess";
     }
 
     @RequestMapping(value = "/no", method = RequestMethod.POST)
-    public String triedAgain(Model model){
+    public String triedAgain(Model model) {
         int generatedAgain = numberService.random();
         model.addAttribute("generatedAgain", generatedAgain);
         return "tryagain";
     }
 
-    @RequestMapping("/home")
-    public String home(){ return "home";}
 
-   @RequestMapping(value = "/firstGuess", method = RequestMethod.GET)
-   public String firstGues(){return "firstGuess";}
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/firstGuess", method = RequestMethod.GET)
+    public String firstGues() {
+        return "firstGuess";
+    }
 
     @RequestMapping(value = "/no", method = RequestMethod.GET)
-    public String answeredNo(){return "no";}
+    public String answeredNo() {
+        return "no";
+    }
 
     @RequestMapping(value = "/yes", method = RequestMethod.GET)
-    public String answeredYes(){return "yes";}
+    public String answeredYes() {
+        return "yes";
+    }
 
-    @RequestMapping(value = "/tryagain", method = RequestMethod.GET)
-    public String tryAgain(){return "tryagain";}
-
-    @RequestMapping(value = "/failed", method = RequestMethod.GET)
-    public String failed(){return "failed";}
-
-
+    @RequestMapping(value = "/failed", method = RequestMethod.POST)
+    public String failed() {
+        return "failed";
+    }
 
 
 }
