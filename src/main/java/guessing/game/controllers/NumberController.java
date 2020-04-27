6df855496f6ec77a0eab7c543904f3219105fa5b
Bartosz <mierzwinski.b@gmail.com>
2020-04-27 18:02:12 +0200

    updating dependencies versions for heroku deployment try5

--- src/main/java/guessing/game/controllers/NumberController.java
+++ src/main/java/guessing/game/controllers/NumberController.java
@@ -1,58 +1,66 @@
 package guessing.game.controllers;
 
 import guessing.game.service.NumberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class NumberController{
+public class NumberController {
 
 
-    @Autowired
     NumberService numberService;
 
-    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    private NumberController(NumberService numberService) {
+        this.numberService = numberService;
+    }
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    public String getHome() {
+        return "home";
+    }
+
+    @RequestMapping(value = "home", method = RequestMethod.POST)
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/firstGuess", method = RequestMethod.POST)
     public String firstResult(Model model) {
         int generatedNumber = numberService.random();
         model.addAttribute("generatedNumber", generatedNumber);
         return "firstGuess";
     }
 
-    @RequestMapping(value = "/no", method = RequestMethod.POST)
+    @RequestMapping(value = "no", method = RequestMethod.POST)
+    public String failed() {
+        return "no";
+    }
+
+    @RequestMapping(value = "failed", method = RequestMethod.POST)
+    public String answeredNo() {
+        return "failed";
+    }
+
+    @RequestMapping(value = "tryagain", method = RequestMethod.POST)
     public String triedAgain(Model model) {
         int generatedAgain = numberService.random();
         model.addAttribute("generatedAgain", generatedAgain);
         return "tryagain";
     }
 
-
-    @RequestMapping("/home")
-    public String home() {
-        return "home";
-    }
-
-    @RequestMapping(value = "/firstGuess", method = RequestMethod.GET)
-    public String firstGues() {
-        return "firstGuess";
-    }
-
-    @RequestMapping(value = "/no", method = RequestMethod.GET)
-    public String answeredNo() {
-        return "no";
-    }
-
-    @RequestMapping(value = "/yes", method = RequestMethod.GET)
+    @RequestMapping(value = "yes", method = RequestMethod.POST)
     public String answeredYes() {
         return "yes";
     }
 
-    @RequestMapping(value = "/failed", method = RequestMethod.POST)
-    public String failed() {
-        return "failed";
-    }
+
+//    @RequestMapping(value = "/no", method = RequestMethod.GET)
+//    public String answeredNo() {
+//        return "no";
+//    }
+//
 
 
 }
