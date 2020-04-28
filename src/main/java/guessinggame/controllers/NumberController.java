e39ae6060d5fe3e1ca30a387ab472baf06617b6a
Bartosz <mierzwinski.b@gmail.com>
2020-04-28 23:30:49 +0200

    omg2

--- src/main/java/guessinggame/controllers/NumberController.java
+++ src/main/java/guessinggame/controllers/NumberController.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class NumberController {
 
 
